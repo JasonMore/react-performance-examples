6103b913b6caaa3cf2435580971490cf199a85a6
Jason More <jasonmore@github.com>
2025-09-23 15:05:01 -0500

    css cleanup

--- src/zustand/exampleComponents/worldSelector/WorldIdButton.tsx
+++ src/zustand/exampleComponents/worldSelector/WorldIdButton.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { useWorldStore } from "../../data/WorldStore";
-import css from "./WorldIdItem.module.css";
+import css from "./WorldIdButton.module.css";
 import { RenderToken } from "../../RenderToken";
 
 export const WorldIdButton = memo(function WorldIdItem({ id }: { id: string }) {
@@ -10,7 +10,7 @@ export const WorldIdButton = memo(function WorldIdItem({ id }: { id: string }) {
     <li>
       <button
         type="button"
-        className={[css.worldButton, isSelected ? css.selected : ""].join(" ")}
+        className={`${css.worldButton} ${isSelected ? css.selected : ""}`}
         onClick={() => setEditId(id)}
       >
         {id}
