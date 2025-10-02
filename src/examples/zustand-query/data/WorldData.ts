c7228d7cb990966033e30356418be8c90fc325a4
Jason More <jasonmore@github.com>
2025-10-02 16:41:34 -0400

    rename

--- src/examples/zustand-query/data/WorldData.ts
+++ src/examples/zustand-query/data/WorldData.ts
@@ -1,28 +1,29 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { fetchWorlds, addWorld } from "../../../api/worlds.ts";
 
+const queryKey = ["worlds"] as const;
+
 // TODO: This is used for typescript, look up a better way?
 const getWorldsQueryOptions = () => ({
-  queryKey: ["worlds"] as const,
+  queryKey,
   queryFn: fetchWorlds,
   refetchOnWindowFocus: false,
 });
 
 export const useGetWorlds = () => useQuery(getWorldsQueryOptions());
 
-export const useGetWorld = (id: string) => {
-  return useQuery({
+export const useGetWorld = (id: string) =>
+  useQuery({
     ...getWorldsQueryOptions(),
     select: ({ worlds }) => worlds.find((w) => w.id === id),
-  }).data;
-};
+  });
 
 export const useAddWorld = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: addWorld,
     onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: ["worlds"] });
+      queryClient.invalidateQueries({ queryKey });
     },
   });
 };
